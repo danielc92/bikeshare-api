b1768c9e6f9e9655c8b3ca96e80449ca711678be
Daniel Corcoran <danielc.92@outlook.com>
2020-06-17 20:13:00 +1000

    Created a bike model, deleted boilerplate

---
+++ src/entity/Bike.ts
@@ -0,0 +1,32 @@
+import  { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class Bike {
+
+    @PrimaryGeneratedColumn()
+    id :number;
+
+    @Column()
+    isAvailable: boolean;
+
+    @Column()
+    modelCode: string;
+
+    @Column({
+        enum: [
+            "MALE",
+            "FEMALE"
+        ]
+    })
+    gender: string;
+
+    @Column({
+        enum: [
+            "BLUE",
+            "RED",
+            "YELLOW",
+            "PURPLE",
+        ]
+    })
+    colour: string;
+}
