6684db22f70c05e9dcd4b2772a0164eb7c8ce1f3
Daniel Corcoran <danielc.92@outlook.com>
2020-06-20 12:33:34 +1000

    Test out many to many relationships

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,54 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { Brand, Bike, Feedback, Route, Rider } from "./entity";
+import { createConnection, ConnectionManager, getRepository } from "typeorm";
+import { Brand, Bike, Feedback, Route, Rider, Contact, Pack } from "./entity";
 import * as express from "express";
 import * as faker from "faker";
+import { connect } from "net";
+import { EnumForEnquiry } from "./entity/Contact";
 
 createConnection()
   .then(async (connection) => {
+    // let r = new Rider();
+    // r.city = "sunshine";
+    // r.firstName = "george";
+    // r.lastName = "smith";
+    // r.phone = "04 0000 0000";
+
+    // let packs = connection.getRepository(Pack);
+    // let results = await packs.find();
+
+    // r.packs = results;
+    // await connection.manager.save(r);
+
+    let x = await connection
+      .getRepository(Rider)
+      .findOne({ where: { id: 3 }, relations: ["packs"] });
+    console.log(x);
+
+    x.packs = x.packs.filter((p) => p.id !== 3);
+    await connection.manager.save(x);
+
+    // const data = await connection
+    //   .getRepository(Pack)
+    //   .find({ relations: ["riders"], loadRelationIds: true });
+    // const data = await connection
+    //   .getRepository(Pack)
+    //   .createQueryBuilder("pack")
+    //   .leftJoinAndSelect("pack.riders", "rider")
+    //   .getMany();
+    // console.log(data);
+    // let p = new Pack();
+    // p.rider = r;
+    // p.packName = "Daniels Cool Pack";
+
+    // let p2 = new Pack();
+
+    // p2.rider = r;
+    // p2.packName = "Daniels Old People Pack";
+
+    // await connection.manager.save(r);
+    // await connection.manager.save(p);
+    // await connection.manager.save(p2);
     // a.forEach((x) => {
     //   let z = new Feedback();
     //   z.content = faker.random.words(2);
@@ -14,22 +57,30 @@ createConnection()
     //     .then((s) => console.log("Success"))
     //     .catch((e) => console.log(e));
     // });
-    const app = express();
-    app.get(
-      "/",
-      (
-        request: express.Request,
-        response: express.Response,
-        next: express.NextFunction
-      ) => {
-        const repo = connection.manager.getRepository(Feedback);
-        repo
-          .find({ take: 10 })
-          .then((data) => response.status(200).json({ data }))
-          .catch((error) => response.status(404).json({ error }));
-      }
-    );
-    app.listen(3010);
+
+    // connection.manager
+    //   .getRepository(Contact)
+    //   .find()
+    //   .then((r) => console.log(r));
+
+    // console.log(EnumForEnquiry['C'])
+
+    // const app = express();
+    // app.get(
+    //   "/",
+    //   (
+    //     request: express.Request,
+    //     response: express.Response,
+    //     next: express.NextFunction
+    //   ) => {
+    //     const repo = connection.manager.getRepository(Feedback);
+    //     repo
+    //       .find({ take: 10 })
+    //       .then((data) => response.status(200).json({ data }))
+    //       .catch((error) => response.status(404).json({ error }));
+    //   }
+    // );
+    // app.listen(3010);
   })
   .catch((error) => console.log(error));
 
