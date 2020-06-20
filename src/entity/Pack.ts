65f54dedadc6e8c9ed641669d6f46ec5d545fcf8
Daniel Corcoran <danielc.92@outlook.com>
2020-06-20 12:36:20 +1000

    Added foreign  relationship to Rider and Many To Many relationship to Rider

--- src/entity/Pack.ts
+++ src/entity/Pack.ts
@@ -3,12 +3,25 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  Column,
+  ManyToOne,
+  ManyToMany,
 } from "typeorm";
+import { Rider } from "./Rider";
 
 @Entity()
 export class Pack {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  packName: string;
+
+  @ManyToOne((type) => Rider, (rider) => rider.myPacks)
+  rider: Rider;
+
+  @ManyToMany((type) => Rider, (rider) => rider.packs)
+  riders: Rider[];
 
   @UpdateDateColumn()
   modifiedAt: Date;
