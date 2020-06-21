fcea5704392fa0cef23c7cde48dadbf1199a078f
Daniel Corcoran <danielc.92@outlook.com>
2020-06-21 18:41:16 +1000

    Modify all routes

--- src/allRoutes.ts
+++ src/allRoutes.ts
@@ -1,11 +1,30 @@
 import { bikeGetAllAction } from "./controllers/BikeGetAllAction";
+import { bikeGetOneAction } from "./controllers/BikeGetOneAction";
 import { authMiddlewareFunc } from "./middleware/AuthDummyUser";
 
+const METHODS = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  DELETE: "delete",
+  PATCH: "patch",
+};
+
+const ROUTES = {
+  BIKE_ALL: "/bike",
+  BIKE_DETAIL: "/bike/detail",
+};
 export const AppRoutes = [
   {
-    path: "/api/bikes",
-    method: "get",
+    path: ROUTES.BIKE_ALL,
+    method: METHODS.GET,
     action: bikeGetAllAction,
     middlewares: [authMiddlewareFunc],
+  },
+  {
+    path: ROUTES.BIKE_DETAIL,
+    method: METHODS.GET,
+    action: bikeGetOneAction,
+    middlewares: [],
   },
 ];
