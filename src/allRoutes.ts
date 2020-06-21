f060c199c96eec6125c4f461c3d6edbe2d74cd86
Daniel Corcoran <danielc.92@outlook.com>
2020-06-21 19:47:02 +1000

    Added Post route for bike

--- src/allRoutes.ts
+++ src/allRoutes.ts
@@ -1,6 +1,8 @@
 import { bikeGetAllAction } from "./controllers/BikeGetAllAction";
 import { bikeGetOneAction } from "./controllers/BikeGetOneAction";
+import { bikeDeleteOneAction } from "./controllers/BikeDeleteOneAction";
 import { authMiddlewareFunc } from "./middleware/AuthDummyUser";
+import { bikeCreateOneAction } from "./controllers/BikeCreateOneAction";
 
 const METHODS = {
   GET: "get",
@@ -11,20 +13,32 @@ const METHODS = {
 };
 
 const ROUTES = {
-  BIKE_ALL: "/bike",
+  BIKE: "/bike",
   BIKE_DETAIL: "/bike/detail",
 };
 export const AppRoutes = [
   {
-    path: ROUTES.BIKE_ALL,
+    path: ROUTES.BIKE,
     method: METHODS.GET,
     action: bikeGetAllAction,
-    middlewares: [authMiddlewareFunc],
+    middlewares: [],
   },
   {
     path: ROUTES.BIKE_DETAIL,
     method: METHODS.GET,
     action: bikeGetOneAction,
+    middlewares: [],
+  },
+  {
+    path: ROUTES.BIKE,
+    method: METHODS.DELETE,
+    action: bikeDeleteOneAction,
+    middlewares: [],
+  },
+  {
+    path: ROUTES.BIKE,
+    method: METHODS.POST,
+    action: bikeCreateOneAction,
     middlewares: [],
   },
 ];
