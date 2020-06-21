4f0e76a8b15e19d10361851411ddd9f80aa59365
Daniel Corcoran <danielc.92@outlook.com>
2020-06-21 19:47:26 +1000

    Created BikeCreateOneAction

---
+++ src/controllers/BikeCreateOneAction.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Bike } from "../entity";
+
+export async function bikeCreateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { colour, gender, modelCode, isAvailable } = request.body;
+    const bike = new Bike();
+    bike.colour = colour;
+    bike.isAvailable = isAvailable;
+    bike.modelCode = modelCode;
+    bike.gender = gender;
+    const results = await getManager().save(bike);
+    response.send(results);
+  } catch (error) {
+    console.log(error);
+    response.status(400).json({ error: error.name });
+  }
+}
