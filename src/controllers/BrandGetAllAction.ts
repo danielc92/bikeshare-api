f32fc300425de20ebb19c1f9efb2ec6637a4fd6d
Daniel Corcoran <danielc.92@outlook.com>
2020-06-23 22:05:28 +1000

    Create BrandGetAllAction

---
+++ src/controllers/BrandGetAllAction.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Route, Brand } from "../entity";
+
+export async function brandGetAllAction(request: Request, response: Response) {
+  const repo = getManager().getRepository(Brand);
+  const results = await repo.find();
+  return response.status(200).json({
+    results,
+  });
+}
