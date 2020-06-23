7076249ba8254bf86be9b9329e286e5379f02c2e
Daniel Corcoran <danielc.92@outlook.com>
2020-06-23 22:06:35 +1000

    Create RouteGetAllAction

---
+++ src/controllers/RouteGetAllAction.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Route } from "../entity";
+
+export async function routeGetAllAction(request: Request, response: Response) {
+  const repo = getManager().getRepository(Route);
+  const results = await repo.find();
+  return response.status(200).json({
+    results,
+  });
+}
