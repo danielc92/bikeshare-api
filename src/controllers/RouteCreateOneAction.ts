49f6034aa99b96f6d75fbb7086f12709c99d9daf
Daniel Corcoran <danielc.92@outlook.com>
2020-06-23 22:06:05 +1000

    Create RouteCreateOneAction

---
+++ src/controllers/RouteCreateOneAction.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Route } from "../entity";
+
+export async function routeCreateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { area, difficulty, totalDistance } = request.body;
+
+    const newRecord = new Route();
+    newRecord.area = area;
+    newRecord.difficulty = difficulty;
+    newRecord.totalDistance = totalDistance;
+
+    const results = await getManager().save(newRecord);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ error: error.toString() });
+  }
+}
