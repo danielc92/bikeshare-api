2d58e6a89ce8ad3aa4eb0efff85cd0531a747b4f
Daniel Corcoran <danielc.92@outlook.com>
2020-06-23 22:05:00 +1000

    Create BikeUpdateOneAction

---
+++ src/controllers/BikeUpdateOneAction.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Bike } from "../entity";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+
+export async function bikeUpdateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { id, colour, isAvailable, brand, modelCode, gender } = request.body;
+    if (!id) return response.status(400).json({ message: "Missing id." });
+
+    const repo = getManager().getRepository(Bike);
+
+    let partial: QueryDeepPartialEntity<Bike> = {};
+    if (colour) partial.colour = colour;
+    if (brand) partial.brand = brand;
+    if (isAvailable) partial.isAvailable = isAvailable;
+    if (modelCode) partial.modelCode = modelCode;
+    if (gender) partial.gender = gender;
+
+    const results = await repo.update(id as string, partial);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ message: error.toString() });
+  }
+}
