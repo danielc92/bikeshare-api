73379f682c2644173835e8023d6852488fb94671
Daniel Corcoran <danielc.92@outlook.com>
2020-06-23 22:05:47 +1000

    Create BrandUpdateOneAction

---
+++ src/controllers/BrandUpdateOneAction.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Bike, Route, Brand } from "../entity";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+
+export async function brandUpdateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { id, title } = request.body;
+    if (!id) return response.status(400).json({ message: "Missing id." });
+
+    const repo = getManager().getRepository(Brand);
+
+    let partial: QueryDeepPartialEntity<Brand> = {};
+    if (title) partial.title = title;
+
+    const results = await repo.update(id as string, partial);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ message: error.toString() });
+  }
+}
