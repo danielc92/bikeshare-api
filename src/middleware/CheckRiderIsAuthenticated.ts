3a31decf215648e50688606246bcb8d35b553718
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:43:21 +1000

    Rename auth middleware

---
+++ src/middleware/CheckRiderIsAuthenticated.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Response, Request } from "express";
+import * as jwt from "jsonwebtoken";
+
+export async function authMiddlewareFunc(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const token = request.headers["token"];
+    if (!token) {
+      return response.status(400).json({ message: "Missing auth token" });
+    }
+
+    const decoded = await jwt.verify(token as string, "secret");
+    next();
+  } catch (error) {
+    response.status(400).json({ message: error.toString() });
+  }
+}
