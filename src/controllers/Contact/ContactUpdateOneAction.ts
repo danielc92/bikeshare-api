d79216c9fb0c54466fd51469db57a3c8e2122f1a
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:39:51 +1000

    Created ContactUpdateOneAction controller

---
+++ src/controllers/Contact/ContactUpdateOneAction.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Contact } from "@entity";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+
+export async function contactUpdateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { id, enquiryContent, enquiryType } = request.body;
+    if (!id) return response.status(400).json({ message: "Missing id." });
+
+    const repo = getManager().getRepository(Contact);
+
+    let partial: QueryDeepPartialEntity<Contact> = {};
+    if (enquiryContent) partial.enquiryContent = enquiryContent;
+    if (enquiryType) partial.enquiryType = enquiryType;
+
+    const results = await repo.update(id as string, partial);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ message: error.toString() });
+  }
+}
