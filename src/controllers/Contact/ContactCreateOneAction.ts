029f334307c8f5501b63c6466fbe06a4a3f45bc2
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:39:20 +1000

    Created ContactCreateOneAction controller

---
+++ src/controllers/Contact/ContactCreateOneAction.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Rider, Contact } from "@entity";
+
+export async function contactCreateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { enquiryContent, enquiryType } = request.body;
+
+    const newRecord = new Contact();
+    newRecord.enquiryContent = enquiryContent;
+    newRecord.enquiryType = enquiryType;
+
+    const results = await getManager().save(newRecord);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ error: error.toString() });
+  }
+}
