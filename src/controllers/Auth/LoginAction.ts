7908f15f93028232db06e2f73a9ad82fbf6abc12
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:39:02 +1000

    Created LoginAction controller

---
+++ src/controllers/Auth/LoginAction.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Rider } from "../../entity";
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+import { brandCreateOneAction } from "controllers/Brand/BrandCreateOneAction";
+export async function loginAction(request: Request, response: Response) {
+  try {
+    const { email, password } = request.body;
+    const repo = getManager().getRepository(Rider);
+
+    const rider = await repo.findOne({
+      where: {
+        email,
+      },
+    });
+    const hashedPassword = await bcrypt.compare(password, rider.password);
+    if (!hashedPassword)
+      return response.status(400).json({ message: "Incorrect credentials" });
+
+    const token = await jwt.sign(
+      {
+        email,
+      },
+      "secret",
+      {
+        expiresIn: 3600,
+      }
+    );
+    return response.status(200).json({
+      token,
+    });
+  } catch (error) {
+    return response.status(400).json({ message: error.toString() });
+  }
+}
