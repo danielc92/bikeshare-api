a3476e416e5f6631e648dca6e1c3cf86efc4041d
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:39:36 +1000

    Created ContactGetAllAction controller

---
+++ src/controllers/Contact/ContactGetAllAction.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Contact } from "@entity";
+
+export async function contactGetAllAction(
+  request: Request,
+  response: Response
+) {
+  const repo = getManager().getRepository(Contact);
+  const results = await repo.find();
+  return response.status(200).json({
+    results,
+  });
+}
