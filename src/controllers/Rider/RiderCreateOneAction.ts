675708f7229d770b7b93e08ae49fb366b7243439
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 20:41:04 +1000

    Hash password

--- src/controllers/Rider/RiderCreateOneAction.ts
+++ src/controllers/Rider/RiderCreateOneAction.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from "express";
 import { getManager } from "typeorm";
 import { Rider } from "../../entity";
-
+import * as bcrypt from "bcrypt";
 export async function riderCreateOneAction(
   request: Request,
   response: Response
 ) {
   try {
-    const { city, firstName, lastName, phone } = request.body;
+    const { city, firstName, lastName, phone, password, email } = request.body;
 
     const newRecord = new Rider();
     newRecord.city = city;
     newRecord.firstName = firstName;
     newRecord.lastName = lastName;
     newRecord.phone = phone;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    newRecord.password = hashedPassword;
+    newRecord.email = email;
 
     const results = await getManager().save(newRecord);
     return response.send(results);
