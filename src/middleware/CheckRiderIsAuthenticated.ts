9e18399b7170bb7e3e5574f2016f489fc81e069d
Daniel Corcoran <danielc.92@outlook.com>
2020-06-24 21:54:09 +1000

    Added IDecodedUser interface

--- src/middleware/CheckRiderIsAuthenticated.ts
+++ src/middleware/CheckRiderIsAuthenticated.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Response, Request } from "express";
 import * as jwt from "jsonwebtoken";
 
+interface IDecodedRider {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+jwt.TokenExpiredError;
 export async function authMiddlewareFunc(
   request: Request,
   response: Response,
@@ -13,7 +20,7 @@ export async function authMiddlewareFunc(
     }
 
     const decoded = await jwt.verify(token as string, "secret");
-    request.user = decoded;
+    request.user = decoded as IDecodedRider;
     next();
   } catch (error) {
     return response.status(400).json({ message: error.toString() });
