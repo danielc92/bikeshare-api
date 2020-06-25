f401a7dd5faaf9474e2fe5094876eab619dbfdf5
Daniel Corcoran <danielc.92@outlook.com>
2020-06-25 21:12:38 +1000

    Created PackCreateOneAction controller

---
+++ src/controllers/Pack/PackCreateOneAction.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Pack, Rider } from "../../entity";
+
+export async function packCreateOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { packMotto, packName } = request.body;
+
+    const newRecord = new Pack();
+    newRecord.packMotto = packMotto;
+    newRecord.packName = packName;
+
+    const rider = await getManager()
+      .getRepository(Rider)
+      .findOne(request.user.id);
+    newRecord.rider = rider;
+
+    const results = await getManager().save(newRecord);
+    return response.send(results);
+  } catch (error) {
+    return response.status(400).json({ error: error.toString() });
+  }
+}
