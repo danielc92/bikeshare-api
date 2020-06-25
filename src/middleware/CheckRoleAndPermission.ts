363aa1de5d3da96657670b6fd54d0bcbca21d3c5
Daniel Corcoran <danielc.92@outlook.com>
2020-06-25 17:02:52 +1000

    Finish logic for permission middleware

--- src/middleware/CheckRoleAndPermission.ts
+++ src/middleware/CheckRoleAndPermission.ts
@@ -1,17 +1,31 @@
 import { NextFunction, Response, Request } from "express";
 import { getManager } from "typeorm";
 import { Role } from "../entity";
+import { RoleEnum } from "../entity/Role";
 export async function roleAndPermissionMiddleware(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
   try {
-    console.log(request.user, "Roles");
+    const role = !request.user ? RoleEnum.ANON : request.user.role;
 
     const repo = getManager().getRepository(Role);
+    const result = await repo.findOne({
+      where: { role },
+      relations: ["permissions"],
+    });
 
-    // const roleExist = await repo.find({ role: request });
+    const allowedToAccess = result.permissions.some(
+      (p) =>
+        p.requestMethod.toLowerCase() === request.method.toLowerCase() &&
+        p.apiRoute.toLowerCase() === request.path.toLowerCase()
+    );
+
+    if (!allowedToAccess)
+      return response
+        .status(400)
+        .json({ message: "No permission to resource" });
 
     next();
   } catch (error) {
