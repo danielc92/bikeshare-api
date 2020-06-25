a3a4dff4ab5983b0dd525103968daa371ed1e98f
Daniel Corcoran <danielc.92@outlook.com>
2020-06-25 16:04:35 +1000

    Load relation ids on rider gets

--- src/controllers/Rider/RiderGetAllAction.ts
+++ src/controllers/Rider/RiderGetAllAction.ts
@@ -4,7 +4,7 @@ import { Rider } from "../../entity";
 
 export async function riderGetAllAction(request: Request, response: Response) {
   const repo = getManager().getRepository(Rider);
-  const results = await repo.find();
+  const results = await repo.find({ loadRelationIds: true });
   return response.status(200).json({
     results,
   });
--- src/controllers/Rider/RiderGetOneAction.ts
+++ src/controllers/Rider/RiderGetOneAction.ts
@@ -7,7 +7,7 @@ export async function riderGetOneAction(request: Request, response: Response) {
     const { id } = request.query;
     if (!id) return response.status(400).json({ message: "Missing id." });
     const repo = getManager().getRepository(Rider);
-    const results = await repo.findOne(id as string);
+    const results = await repo.findOne(id as string, { loadRelationIds: true });
     if (!results) {
       return response.status(404).json({ message: "Resource not found." });
     }
