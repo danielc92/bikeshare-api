de9a83f27eaeca1ad5d0423fcf4e85553b154f3b
Daniel Corcoran <danielc.92@outlook.com>
2020-06-25 18:17:51 +1000

    Modified Contact entity

--- src/entity/Contact.ts
+++ src/entity/Contact.ts
@@ -6,16 +6,30 @@ import {
   Column,
 } from "typeorm";
 
+enum EnquiryTypeEnum {
+  COMPLAINT = "Complaint",
+  PRAISE = "Praise",
+  SUGGESTION = "Suggestion",
+  OTHER = "Other",
+}
+
 @Entity()
 export class Contact {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  enquiryContent: string;
+  email: string;
 
   @Column()
-  enquiryType: string;
+  enquiryContent: string;
+
+  @Column({
+    type: "enum",
+    enum: EnquiryTypeEnum,
+    default: EnquiryTypeEnum.OTHER,
+  })
+  enquiryType: EnquiryTypeEnum;
 
   @UpdateDateColumn()
   modifiedAt: Date;
