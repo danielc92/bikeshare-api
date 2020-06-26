622b6ee9b82f1cbc55781f1ed7f5d577da8b87fd
Daniel Corcoran <danielc.92@outlook.com>
2020-06-26 18:53:30 +1000

    Created MyRoutesAddOneAction controller

---
+++ src/controllers/MyRoutes/MyRoutesAddOneAction.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { getManager, createQueryBuilder } from "typeorm";
+import { Rider, Route } from "../../entity";
+
+export async function myRoutesAddOneAction(
+  request: Request,
+  response: Response
+) {
+  try {
+    const { routeId } = request.body;
+    const routeRepo = getManager().getRepository(Route);
+    const route = await routeRepo.findOne(routeId);
+    if (!route)
+      return response.status(400).json({ message: "Resource not found" });
+
+    const riderRepo = getManager().getRepository(Rider);
+    const rider = await riderRepo.findOne(request.user.id);
+    if (!rider)
+      return response.status(400).json({ message: "Resource not found" });
+
+    await createQueryBuilder().relation(Rider, "routes").of(rider).add(route);
+
+    return response.status(200).json({ added: route });
+  } catch (error) {
+    console.log(error);
+    return response.status(400).json({ message: error.toString() });
+  }
+}
