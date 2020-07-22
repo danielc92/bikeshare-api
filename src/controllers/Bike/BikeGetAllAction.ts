b5a8edbae7f4ec39297da770ca0fe975ec6334dd
Daniel Corcoran <danielc.92@outlook.com>
2020-07-22 17:19:21 +1000

    Tested clustering and redis

--- src/controllers/Bike/BikeGetAllAction.ts
+++ src/controllers/Bike/BikeGetAllAction.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from "express";
 import { getManager } from "typeorm";
 import { Bike } from "../../entity";
+import { client } from "../../redis_client";
 
 export async function bikeGetAllAction(request: Request, response: Response) {
   try {
-    const repo = getManager().getRepository(Bike);
-    const results = await repo.find();
-    return response.status(200).json({
-      results,
+    client.get("allBikes", async (error, reply) => {
+      if (!reply) {
+        const repo = getManager().getRepository(Bike);
+        const results = await repo.find();
+        const resultsString = JSON.stringify(results);
+        client.setex("allBikes", 10000, resultsString, (setError, setReply) => {
+          return response.status(200).json({
+            results,
+          });
+        });
+      } else {
+        return response.status(200).json({
+          results: reply,
+        });
+      }
     });
+
+    // const repo = getManager().getRepository(Bike);
+    // const results = await repo.find();
+    // return response.status(200).json({
+    //   results,
+    // });
   } catch (error) {
     return response.status(400).json({ message: error.toString() });
   }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
+import * as logger from "morgan";
+import * as helmet from "helmet";
+import * as cors from "cors";
 import { AppRoutes } from "./allRoutes";
+import { client } from "./redis_client";
+import * as fs from "fs";
+import * as path from "path";
+
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "morgan_logs/access.log"),
+  { flags: "a" }
+);
+
 createConnection()
   .then(async (connection) => {
+    // Create express app instance
     const app = express();
+
+    // Apply third party middlewares
+    app.use(helmet());
+    app.use(
+      logger("combined", {
+        stream: accessLogStream,
+      })
+    );
+    app.use(cors());
     app.use(express.json());
+
+    // Dynamically apply all routes
     AppRoutes.forEach((route) => {
       app[route.method](
         route.path,
@@ -13,7 +37,7 @@ createConnection()
         (
           request: express.Request,
           response: express.Response,
-          next: Function
+          next: express.NextFunction
         ) => {
           route
             .action(request, response)
@@ -22,6 +46,64 @@ createConnection()
         }
       );
     });
+
+    // Listen for redis client error
+    client.on("error", function (error) {
+      console.error(error);
+    });
+
     app.listen(3050);
   })
   .catch((error) => console.log(error));
+
+// import "reflect-metadata";
+// import { createConnection } from "typeorm";
+// import * as express from "express";
+// import { AppRoutes } from "./allRoutes";
+// import { client } from "./redis_client";
+// import * as cluster from "cluster";
+
+// createConnection()
+//   .then(async (connection) => {
+//     if (cluster.isMaster) {
+//       console.log(`Master ${process.pid} is running.`);
+
+//       for (let i = 0; i < 16; i++) {
+//         cluster.fork();
+//       }
+
+//       cluster.on("online", (worker: cluster.Worker) => {
+//         console.log(`Worker is online ${worker.process.pid}`);
+//       });
+//       cluster.on(
+//         "exit",
+//         (worker: cluster.Worker, code: number, signal: string) => {
+//           console.log(`Worker ${worker.process.pid} died`);
+//         }
+//       );
+//     } else {
+//       const app = express();
+//       app.use(express.json());
+//       AppRoutes.forEach((route) => {
+//         app[route.method](
+//           route.path,
+//           route.middlewares,
+//           (
+//             request: express.Request,
+//             response: express.Response,
+//             next: Function
+//           ) => {
+//             route
+//               .action(request, response)
+//               .then(() => next)
+//               .catch((err) => next(err));
+//           }
+//         );
+//       });
+//       client.on("error", function (error) {
+//         console.error(error);
+//       });
+//       app.listen(3050);
+//     }
+//   })
+//   .catch((error) => console.log(error));
