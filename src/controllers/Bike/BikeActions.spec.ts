0af757064e19a4b21c029dace99e46ec943cd7b5
Daniel Corcoran <danielc.92@outlook.com>
2020-07-26 20:54:54 +1000

    Corrected testing

--- src/controllers/Bike/BikeActions.spec.ts
+++ src/controllers/Bike/BikeActions.spec.ts
@@ -1,29 +1,45 @@
 import { app } from "../../app";
 import { ApiRouteEnum } from "../../entity/Permission";
-import * as supertest from "supertest";
+import * as request from "supertest";
 import { createConnection, getConnection } from "typeorm";
 import { connection } from "../../utils/connection";
 
-describe("Bike tests", async () => {
-  //   beforeAll(async () => {
-  //     await connection.create();
-  //   });
+describe("all of em", () => {
+  beforeAll(async (done) => {
+    console.log("BEFORE ALL");
+    await createConnection();
+    done();
+  });
 
-  //   afterAll(async () => {
-  //     await connection.close();
-  //   });
+  afterAll(async (done) => {
+    console.log("AFTER ALL");
+    await getConnection().close();
+  });
 
-  //   beforeEach(async () => {
-  //     await connection.clear();
-  //   });
-
-  it("Unauthenticated user can access bike list successfully", async () => {
-    createConnection()
-      .then(async (connection) => {
-        const superapp = supertest(app);
-        const result = await superapp.get(ApiRouteEnum.BIKE);
-        return expect(result.status).toEqual(200);
-      })
-      .catch((error) => console.log(error));
+  test("Unauthenticated user bike list", async () => {
+    console.log("TESTING ALL");
+    const response = await request(app).get("/bike");
+    expect(response.status).toEqual(200);
   });
 });
+
+// describe("Bike tests", () => {
+//   beforeAll(async () => {
+//     await connection.create();
+//   });
+
+//   afterAll(async () => {
+//     await connection.close();
+//   });
+
+//   beforeEach(async () => {
+//     await connection.clear();
+//   });
+
+//   it("Unauthenticated user can access bike list successfully", async () => {
+//     await createConnection();
+//     const superapp = supertest(app);
+//     const result = await superapp.get(ApiRouteEnum.BIKE);
+//     expect(result.status).toEqual(200);
+//   });
+// });
