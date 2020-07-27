01fab5c4ba959f384434862cb306e40b2daa97cc
Daniel Corcoran <danielc.92@outlook.com>
2020-07-27 21:11:56 +1000

    Created some new tests

--- src/controllers/Bike/BikeActions.spec.ts
+++ src/controllers/Bike/BikeActions.spec.ts
@@ -1,45 +1,68 @@
 import { app } from "../../app";
-import { ApiRouteEnum } from "../../entity/Permission";
 import * as request from "supertest";
 import { createConnection, getConnection } from "typeorm";
-import { connection } from "../../utils/connection";
+import { ApiRouteEnum } from "../../entity/Permission";
+import { populatePermission } from "../../utils/permissions";
+import { Rider } from "../../entity";
 
-describe("all of em", () => {
+let mockUser = {
+  city: "Atlantis",
+  phone: "00654654",
+  firstName: "Daniel",
+  lastName: "Corcoran",
+  password: "12345",
+  email: "daniel-test-user-1@daniel-test-user.com",
+};
+describe("Bike Suite", () => {
   beforeAll(async (done) => {
-    console.log("BEFORE ALL");
     await createConnection();
+    await populatePermission();
     done();
   });
 
   afterAll(async (done) => {
-    console.log("AFTER ALL");
     await getConnection().close();
+    done();
   });
 
   test("Unauthenticated user bike list", async () => {
-    console.log("TESTING ALL");
-    const response = await request(app).get("/bike");
-    expect(response.status).toEqual(200);
+    const response = await request(app).get(ApiRouteEnum.BIKE).expect(200);
+  });
+
+  test("Unauthenticated user cannot create a new bike", async () => {
+    const response = await request(app)
+      .post(ApiRouteEnum.BIKE)
+      .send({ modelCode: "M1" })
+      .expect(400);
+  });
+
+  test("Bike list contains 'results'", async () => {
+    const response = await request(app).get(ApiRouteEnum.BIKE);
+    expect(response.body).toHaveProperty("results");
+  });
+
+  test("User can be created", async () => {
+    await request(app).post(ApiRouteEnum.RIDER).send(mockUser).expect(200);
+  });
+
+  test("User can log in, and can create a new bike instance", async () => {
+    const response = await request(app)
+      .post(ApiRouteEnum.AUTH_LOGIN)
+      .send({ email: mockUser.email, password: mockUser.password });
+
+    expect(response.status).toBe(200);
+
+    const { token } = response.body;
+
+    await request(app)
+      .post(ApiRouteEnum.BIKE)
+      .set({ token })
+      .send({
+        modelCode: "M001",
+        isAvailable: true,
+        colour: "BLUE",
+        gender: "MALE",
+      })
+      .expect(200);
   });
 });
-
-// describe("Bike tests", () => {
-//   beforeAll(async () => {
-//     await connection.create();
-//   });
-
-//   afterAll(async () => {
-//     await connection.close();
-//   });
-
-//   beforeEach(async () => {
-//     await connection.clear();
-//   });
-
-//   it("Unauthenticated user can access bike list successfully", async () => {
-//     await createConnection();
-//     const superapp = supertest(app);
-//     const result = await superapp.get(ApiRouteEnum.BIKE);
-//     expect(result.status).toEqual(200);
-//   });
-// });
