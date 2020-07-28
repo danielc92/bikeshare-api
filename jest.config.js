de71e88702648e2f95ffeb47d61b44599771dafc
Daniel Corcoran <danielc.92@outlook.com>
2020-07-28 21:01:20 +1000

    Modified jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
+// solution from https://github.com/dividab/tsconfig-paths/issues/97
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  // solution from https://github.com/dividab/tsconfig-paths/issues/97
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
